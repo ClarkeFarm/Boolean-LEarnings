65c98988cf3140d4a1a11f54c8884e3daf00a7a4
josephw <josephw@JOE-CLARKE>
2024-08-07 23:05:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pT = "abc123";
+            string pA = "abc123";
+            int logAttempt = 3;
+            int logMax = 5;
+
+
+            bool pizzaTime = (pT == pA && logAttempt < logMax);
+
+            Console.WriteLine(pizzaTime);
+            Console.ReadKey();
+        }
+    }
+}
